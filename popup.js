5823895fd201aae5bd90596a46e6524be81eb2e7
galicians <galicians@gmail.com>
2014-11-29 15:10:17 +0000

    global variables + xhr function

--- popup.js
+++ popup.js
@@ -1,13 +1,44 @@
+var filename
+var videoRecorder
+
 
 
 function gotStream(stream) {
-    console.log("Received local stream");
-  $('#video').attr({"src": URL.createObjectURL(stream) })
-  // $('#video').attr({"muted": true})
-  console.log(URL.createObjectURL(stream))
-  localstream = stream;
+    var mediaStream = stream;
+    console.log("Received  media stream: ", mediaStream);
+  $('#video').attr({"src": URL.createObjectURL(mediaStream) })
+  
+
+
   stream.onended = function() { console.log("Ended"); };
 }
+
+function xhr(url, data, callback) {
+        var request = new XMLHttpRequest();
+        request.onreadystatechange = function() {
+            if (request.readyState == 4 && request.status == 200) {
+                callback(request.responseText);
+            }
+        };
+
+        request.upload.onprogress = function(event) {
+            console.log('request.upload.onprogress')
+        };
+
+        request.upload.onload = function() {
+            console.log('request.upload.onload')
+        };
+        request.open('POST', url);
+        request.send(data);
+    }
+
+
+
+
+
+
+
+
 
 function getUserMediaError() {
   console.log("getUserMedia() failed.");
@@ -21,7 +52,6 @@ function onAccessApproved(id) {
                             chromeMediaSourceId: id } }
   }, gotStream, getUserMediaError);
 }
-
 
 $(function() {
     $('#start').click(function() { console.log('hello user') })
