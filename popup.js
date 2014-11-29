3a7b4f1c7e1b5835d493650cba07afbd8ddd058d
galicians <galicians@gmail.com>
2014-11-29 13:03:20 +0000

    capturing the id of the page selected

--- popup.js
+++ popup.js
@@ -1,9 +1,11 @@
-
+function onAccessApproved(id) {
+  console.log(id)
+}
 
 
 $(function() {
     $('#start').click(function() { console.log('hello user') })
-    chrome.desktopCapture.chooseDesktopMedia(["screen",'window'], function(id) { console.log(id)   })
+    chrome.desktopCapture.chooseDesktopMedia(["screen",'window'], onAccessApproved)
 })
 
 
