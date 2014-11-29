7e2e8c1eb0ab1edd93b5beae0d371d6e684c7fcd
galicians <galicians@gmail.com>
2014-11-29 15:26:05 +0000

    rtc library receives the stream

--- popup.js
+++ popup.js
@@ -1,40 +1,44 @@
-var filename
-var videoRecorder
+var filename;
+var videoRecorder;
+var audioConfig = {};
 
 
 
 function gotStream(stream) {
     var mediaStream = stream;
     console.log("Received  media stream: ", mediaStream);
-  $('#video').attr({"src": URL.createObjectURL(mediaStream) })
-  
-
+    $('#video').attr({"src": URL.createObjectURL(mediaStream) })
+    var videoConfig = { type: 'video' };
+    videoRecorder = RecordRTC(stream, videoConfig);
+    videoRecorder.startRecording();
 
   stream.onended = function() { console.log("Ended"); };
 }
 
 function xhr(url, data, callback) {
-        var request = new XMLHttpRequest();
-        request.onreadystatechange = function() {
-            if (request.readyState == 4 && request.status == 200) {
-                callback(request.responseText);
-            }
-        };
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+        if (request.readyState == 4 && request.status == 200) {
+            callback(request.responseText);
+        }
+    };
 
-        request.upload.onprogress = function(event) {
-            console.log('request.upload.onprogress')
-        };
+    request.upload.onprogress = function(event) {
+        console.log('request.upload.onprogress')
+    };
 
-        request.upload.onload = function() {
-            console.log('request.upload.onload')
-        };
-        request.open('POST', url);
-        request.send(data);
-    }
-
-
+    request.upload.onload = function() {
+        console.log('request.upload.onload')
+    };
+    request.open('POST', url);
+    request.send(data);
+}
 
 
+
+function generateUrlVideo(){
+
+}
 
 
 
